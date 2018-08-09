7506d045fd25c0319da213a8bcc786a257923d98
SparksFullStack <csparks@antioch.edu>
2018-08-09 16:05:12 -0700

    added notes to PostsNew for reference"

--- src/containers/PostsNew.js
+++ src/containers/PostsNew.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    // renderField is a function that returns the view layer of JSX and receives all the inputs (and other values) as properties on the 'field' object
     renderField = (field) => {
         return (
             <div>
@@ -16,16 +17,19 @@ class PostsNew extends Component {
 
     render(){
         return (
+            // all <Field> tags must be wrapped in a <form>
             <form>
+                {/* here the 'label' property is a value that is just being passed and 'name' is the key being assigned to that part of the state. */}
                 <Field 
                     label="Title"
                     name="title"
+                    // the 'component' property is set to the function returning the view layer of JSX
                     component={this.renderField}
                 />
 
                 <Field 
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
 
@@ -39,4 +43,16 @@ class PostsNew extends Component {
     }
 }
 
-export default reduxForm({ form: 'PostsNewForm' })(PostsNew);
+// validate is a funciton that stores any errors in user input and reports them to Redux-Form
+const validate = () => {
+
+}
+
+// the reduxForm function here takes a single object that wires up the forms as well as the validate method
+export default reduxForm({ 
+    // this is the validate method for checking user inputs
+    validate: validate,
+    // 'PostsNewForm' will be the key assigned to the state that contains the values submitted by this form
+    form: 'PostsNewForm' 
+})(PostsNew);
+
